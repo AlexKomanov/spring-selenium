4419c8654d55f7688b0525cfa1c53f2ac2396a69
alexkomanov <ALexko26>
2021-05-15 15:08:00 +0300

    AK - Adding ScreenShot method

---
+++ src/main/java/com/alex/spring/springselenium/util/ScreenShotUtil.java
@@ -0,0 +1,33 @@
+package com.alex.spring.springselenium.util;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+@Component
+public class ScreenShotUtil {
+
+    @Autowired
+    private WebDriver driver;
+
+    @Value("${screenshot.path}")
+    private Path path;
+
+    public void takeScreenShot(final String imageName){
+        File screenshot = ((TakesScreenshot)this.driver).getScreenshotAs(OutputType.FILE);
+        try {
+            FileCopyUtils.copy(screenshot, this.path.resolve(imageName).toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- src/test/java/com/alex/spring/springselenium/swaglabsTests/SwagLabsTest.java
+++ src/test/java/com/alex/spring/springselenium/swaglabsTests/SwagLabsTest.java
@@ -2,6 +2,7 @@ package com.alex.spring.springselenium.swaglabsTests;
 
 import com.alex.spring.springselenium.SpringBaseTestNGTest;
 import com.alex.spring.springselenium.pages.swaglabs.SwagLabsPage;
+import com.alex.spring.springselenium.util.ScreenShotUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,17 +11,21 @@ public class SwagLabsTest extends SpringBaseTestNGTest {
 
     @Autowired
     private SwagLabsPage swagLabsPage;
+    @Autowired
+    private ScreenShotUtil screenShotUtil;
 
     @Test
     public void swagLabsLoginTest(){
         swagLabsPage.goTo();
         Assert.assertTrue(this.swagLabsPage.isLoaded());
+        this.screenShotUtil.takeScreenShot("main-page.png");
 
         this.swagLabsPage.getLoginPage()
                 .loginToApplication("standard_user", "secret_sauce");
 
         Assert.assertTrue(this.swagLabsPage.getMainPage().isLoaded());
         Assert.assertEquals(this.swagLabsPage.getMainPage().getProductsCount(), 6);
+        this.screenShotUtil.takeScreenShot("products-page.png");
         swagLabsPage.sleep(5000);
         this.swagLabsPage.closeBrowser();
     }
